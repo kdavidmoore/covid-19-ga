afa2647c2a9ca418abefd7dea2e4f9866053df16
Keith Moore <keith.m4@gmail.com>
2020-03-23 14:12:19 -0400

    none

--- covid-19-ga.R
+++ covid-19-ga.R
@@ -8,7 +8,7 @@ df$date <- as.Date(as.character(df$date), format="%Y%m%d")
 p <- ggplot(df, aes(x=df$date, y=df$newCases)) +
   geom_bar(stat="identity", fill="steelblue") +
   geom_text(aes(label=df$newCases), vjust=1.6, color="white", size=3.5) +
-  labs(title="Daily New COVID-19 Cases in Georgia", x ="Date", y = "Daily New Cases") +
+  labs(title="Daily New COVID-19 Cases in Georgia", x="Date", y="Daily New Cases") +
   theme_minimal()
 
 p
