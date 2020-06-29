990b78dffbdaa41d87554b2eaa552a00939a56bc
Keith Moore <keith.m4@gmail.com>
2020-06-29 08:53:50 -0400

    plotting death increase

--- covid-19-ga.R
+++ covid-19-ga.R
@@ -5,10 +5,12 @@ library(lubridate)
 
 df$date <- as.Date(as.character(df$date), format="%Y%m%d")
 
-p <- ggplot(df, aes(x=df$date, y=df$positiveIncrease)) +
+df <- df[df$date >= "2020-05-01" & df$date <= "2020-12-01",]
+
+p <- ggplot(df, aes(x=df$date, y=df$deathIncrease)) +
   geom_bar(stat="identity", fill="steelblue") +
-  geom_text(aes(label=df$positiveIncrease), vjust=1.6, color="white", size=2.5) +
-  labs(title="Daily New COVID-19 Cases in Georgia", x="Date", y="Daily New Cases") +
+  geom_text(aes(label=df$deathIncrease), vjust=1.6, color="white", size=2.5) +
+  labs(title="Daily New COVID-19 Deaths in Georgia", x="Date", y="Daily New Deaths") +
   theme_minimal()
 
 p
