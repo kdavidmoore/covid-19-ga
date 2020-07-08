992ebab1ecc93bc3017974b4c840ebfe056244e0
keithmooredev <keith.m4@gmail.com>
2020-07-08 12:34:12 -0400

    added state argument

--- main.py
+++ main.py
@@ -8,18 +8,21 @@ __author__ = "Keith Moore"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
+import sys
 
-def get_data():
+
+def get_data(state):
     import requests
-    base_url = "https://covidtracking.com/api/v1/states/nc/daily.json"
+    base_url = "https://covidtracking.com/api/v1/states/" + state + "/daily.json"
     r = requests.get(base_url)
     return r.json()
 
 
-def write_csv(data):
+def write_csv(data, state):
     import csv
     keys = data[0].keys()
-    with open("covid-19-nc-data-python.csv", "w", newline="") as outfile:
+    filename = "covid-19-" + state + "-data-python.csv"
+    with open(filename, "w", newline="") as outfile:
         dict_writer = csv.DictWriter(outfile, keys)
         dict_writer.writeheader()
         dict_writer.writerows(data)
@@ -27,9 +30,16 @@ def write_csv(data):
 
 def main():
     """ Main entry point of the app """
-    data = get_data()
-    print(data)
-    write_csv(data)
+    args = len(sys.argv)
+    state = 'ga'
+    if args > 1:
+        state = sys.argv[1]
+
+    print("state: " + state)
+
+    data = get_data(state)
+    # print(data)
+    write_csv(data, state)
 
 
 if __name__ == "__main__":
