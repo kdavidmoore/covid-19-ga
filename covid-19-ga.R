b107310d47407a711ace4a0aac41db4f704c85c6
keithmooredev <keith.m4@gmail.com>
2020-08-25 10:39:16 -0400

    added y intercept, changed labels

--- covid-19-ga.R
+++ covid-19-ga.R
@@ -5,12 +5,13 @@ library(lubridate)
 
 df$date <- as.Date(as.character(df$date), format="%Y%m%d")
 
-df <- df[df$date >= "2020-05-01" & df$date <= "2020-12-01",]
+df <- df[df$date >= "2020-05-15" & df$date <= "2020-12-01",]
 
 p <- ggplot(df, aes(x=df$date, y=df$positiveIncrease)) +
   geom_bar(stat="identity", fill="steelblue") +
   geom_text(aes(label=df$positiveIncrease), vjust=1.6, color="white", size=2.5) +
-  labs(title="Daily New COVID-19 Cases in Georgia", x="Date", y="Daily New Cases") +
+  labs(title="COVID-19 Positive Increase in Georgia", x="Date", y="positiveIncrease") +
+  geom_hline(yintercept=3000, color = "red") +
   theme_minimal()
 
 p
