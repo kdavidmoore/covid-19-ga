705f4ef43cb857d2502d7ba7a9e39c621c23173a
keithmooredev <keith.m4@gmail.com>
2020-11-05 18:08:30 -0500

    none

--- covid-19-ga.R
+++ covid-19-ga.R
@@ -5,13 +5,13 @@ library(lubridate)
 
 df$date <- as.Date(as.character(df$date), format="%Y%m%d")
 
-df <- df[df$date >= "2020-05-15" & df$date <= "2020-12-01",]
+df <- df[df$date >= "2020-08-01",]
 
 p <- ggplot(df, aes(x=df$date, y=df$positiveIncrease)) +
   geom_bar(stat="identity", fill="steelblue") +
   geom_text(aes(label=df$positiveIncrease), vjust=1.6, color="white", size=2.5) +
-  labs(title="COVID-19 Positive Increase in Georgia", x="Date", y="positiveIncrease") +
-  geom_hline(yintercept=3000, color = "red") +
+  labs(title="COVID-19 Daily New Cases in Georgia", x="Date", y="positiveIncrease") +
+  geom_hline(yintercept=1000, color = "red") +
   theme_minimal()
 
 p
